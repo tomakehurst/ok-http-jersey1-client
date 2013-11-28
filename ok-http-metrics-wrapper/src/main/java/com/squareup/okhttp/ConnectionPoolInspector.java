fbf636357a0130f0b28194f05258907415fa4f39
tomakehurst <t.m.akehurst@googlemail.com>
2013-11-28 17:43:18 +0000

    Added live and total connection counts  to connection pool inspector

--- ok-http-metrics-wrapper/src/main/java/com/squareup/okhttp/ConnectionPoolInspector.java
+++ ok-http-metrics-wrapper/src/main/java/com/squareup/okhttp/ConnectionPoolInspector.java
@@ -1,5 +1,9 @@
 package com.squareup.okhttp;
 
+import com.google.common.base.Predicate;
+
+import static com.google.common.collect.FluentIterable.from;
+
 public class ConnectionPoolInspector {
 
     private final ConnectionPool connectionPool;
@@ -8,7 +12,19 @@ public class ConnectionPoolInspector {
         this.connectionPool = connectionPool;
     }
 
-    public int activeConnections() {
-        return 0;
+    public int liveConnections() {
+        return countMatching(new Predicate<Connection>() {
+            public boolean apply(Connection connection) {
+                return connection.isAlive();
+            }
+        });
+    }
+
+    public int totalConnections() {
+        return connectionPool.getConnectionCount();
+    }
+
+    private int countMatching(Predicate<Connection> predicate) {
+        return from(connectionPool.getConnections()).filter(predicate).size();
     }
 }
--- ok-http-metrics-wrapper/src/test/java/com/squareup/okhttp/ConnectionPoolInspectorTest.java
+++ ok-http-metrics-wrapper/src/test/java/com/squareup/okhttp/ConnectionPoolInspectorTest.java
@@ -1,15 +1,26 @@
 package com.squareup.okhttp;
 
 import com.github.tomakehurst.wiremock.junit.WireMockRule;
+import com.google.common.collect.FluentIterable;
+import com.google.common.collect.Iterables;
+import com.google.common.io.ByteStreams;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 
-import java.util.concurrent.Executors;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.List;
 
+import static com.github.tomakehurst.wiremock.client.WireMock.*;
+import static com.github.tomakehurst.wiremock.common.Exceptions.throwUnchecked;
 import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.wireMockConfig;
-import static java.util.Arrays.asList;
-import static org.mockito.Mockito.*;
+import static com.google.common.collect.Lists.newArrayList;
+import static org.hamcrest.Matchers.greaterThan;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
 
 public class ConnectionPoolInspectorTest {
 
@@ -18,30 +29,69 @@ public class ConnectionPoolInspectorTest {
     ConnectionPoolInspector connectionPoolInspector;
 
     @Rule
-    public WireMockRule wm = new WireMockRule(wireMockConfig().port(0));
+    public WireMockRule wm = new WireMockRule(wireMockConfig().port(9999));
 
     @Before
     public void init() {
-        connectionPool = new ConnectionPool(2, 5000);
+        connectionPool = new ConnectionPool(2, 50000000);
         connectionPoolInspector = new ConnectionPoolInspector(connectionPool);
 
         client = new OkHttpClient().setConnectionPool(connectionPool);
+
+        wm.stubFor(any(urlEqualTo("/something")).willReturn(aResponse().withStatus(200).withBody("BODY")));
     }
 
     @Test
-    public void correctlyReportsLiveConnections() {
-        Executors.newFixedThreadPool(10).invokeAll()
+    public void correctlyReportsLiveConnections() throws Exception {
+        assertThat(connectionPoolInspector.liveConnections(), is(0));
+
+        runIn10ConcurrentThreadsAndJoin(getTestUrlTask());
+
+        assertThat(connectionPoolInspector.liveConnections(), greaterThan(0));
     }
 
-    private Connection activeConnection() {
-        Connection connection = mock(Connection.class);
-        when(connection.isAlive()).thenReturn(true);
-        return connection;
+    private Runnable getTestUrlTask() {
+        return new Runnable() {
+            public void run() {
+                URL testUrl = url("/something");
+                HttpURLConnection connection = client.open(testUrl);
+                InputStream in = null;
+                try {
+                    in = connection.getInputStream();
+                    ByteStreams.toByteArray(in);
+                } catch (Exception e) {
+                    throwUnchecked(e);
+                } finally {
+                    if (in != null) {
+                        try {
+                            in.close();
+                        } catch (Exception e) {
+                            throwUnchecked(e);
+                        }
+                    }
+                }
+            }
+        };
     }
 
-    private Connection inactiveConnection() {
-        Connection connection = mock(Connection.class);
-        when(connection.isAlive()).thenReturn(false);
-        return connection;
+    private void runIn10ConcurrentThreadsAndJoin(Runnable task) throws InterruptedException {
+        List<Thread> threads = newArrayList();
+        for (int i = 1; i <= 10; i++) {
+            Thread thread = new Thread(task);
+            threads.add(thread);
+            thread.start();
+        }
+
+        for (Thread thread: threads) {
+            thread.join();
+        }
+    }
+
+    private URL url(String pathAndQuery) {
+        try {
+            return new URL("http://localhost:" + wm.port() + pathAndQuery);
+        } catch (MalformedURLException e) {
+            return throwUnchecked(e, URL.class);
+        }
     }
 }
